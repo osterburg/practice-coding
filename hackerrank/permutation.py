fc0000450116e36b0d0fbc4da7067960af8aa74f
Stephan Osterburg <stephanosterburg@gmail.com>
2019-08-08 12:56:53 -0700

    init

--- hackerrank/permutation.py
+++ hackerrank/permutation.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 from itertools import permutations
 
 filename = '/Users/stephanosterburg/Projects/practice-coding/hackerrank/itertools-permutations-testcases/input/input00.txt'
@@ -6,3 +7,13 @@ sample = ["HACK", 2]
 mylist = sorted(list(permutations(sample[0], int(sample[1]))))
 for _ in mylist:
     print(''.join(_))
+
+print('\n')
+
+S, number = ["ABCD", 3]
+print(S, number)
+
+for i in range(1, int(number) + 1):
+    c = sorted(list(combinations(sorted(S), i)))
+    for _ in c:
+        print(''.join(_))

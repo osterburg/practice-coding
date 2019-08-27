191c120bd09b80d6e1b74c6dcd4fc6c582c2fee6
Stephan Osterburg <stephanosterburg@gmail.com>
2019-08-27 12:55:47 -0700

    Update minimumSwaps.py

--- hackerrank/minimumSwaps.py
+++ hackerrank/minimumSwaps.py
@@ -2,7 +2,8 @@ def minimumSwaps(arr):
     swaps = 0
 
     for i in range(len(arr)):
-        # Check if key and value are equal
+        # Check if key-value and value are equal,
+        # if not swap previous and current
         while arr[i] != i + 1:
             temp = arr[arr[i] - 1]
             arr[arr[i] - 1] = arr[i]

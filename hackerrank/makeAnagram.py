3fd4fe77159ce28a5379bb73fc61885e1884b72b
Stephan Osterburg <stephanosterburg@gmail.com>
2019-08-30 10:44:32 -0700

    Update makeAnagram.py

--- hackerrank/makeAnagram.py
+++ hackerrank/makeAnagram.py
@@ -1,17 +1,24 @@
+from collections import Counter
+
 def makeAnagram(a, b):
     count = [0] * 26
     offset = ord('a') # unicode representation of a character, a = 97
 
     for char in a:
     	count[ord(char) - offset] += 1
+
     for char in b:
     	count[ord(char) - offset] -= 1
 
-    total = 0
-    for value in count:
-    	total += abs(value)
+    # total = 0
+    # for value in cnt:
+    #     total += abs(value)
+    #
+    # return total
 
-    return(total)
+    count = map(abs, count)
+
+    return count.count(1)
 
 
 a = 'cde'

d3aa4ae875620de4fe1ecd474a131910ac95c34b
Stephan Osterburg <stephanosterburg@gmail.com>
2019-09-13 10:47:16 -0700

    update

--- hackerrank/pairs.py
+++ hackerrank/pairs.py
@@ -3,6 +3,8 @@
 # a target value of 1, we have three values meeting the condition:
 # 2 -1 = 1, 3 -2 = 1, and 4 -3 = 1
 #
+# Your code did not execute within the time limits
+# when using pairs_input09.txt
 def pairs(k, arr):
     count = 0
     for i in range(len(arr)):
@@ -16,3 +18,21 @@ def pairs(k, arr):
 k = 2
 arr = [1, 5, 3, 4, 2]
 print(pairs(k, arr))  # 3
+
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#
+#     nk = input().split()
+#
+#     n = int(nk[0])
+#
+#     k = int(nk[1])
+#
+#     arr = list(map(int, input().rstrip().split()))
+#
+#     result = pairs(k, arr)
+#
+#     fptr.write(str(result) + '\n')
+#
+#     fptr.close()

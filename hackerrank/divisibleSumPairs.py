df90293afb03abb1a755f5619cfa867e9fedb660
Stephan Osterburg <stephanosterburg@gmail.com>
2019-09-28 11:38:56 -0700

    Update divisibleSumPairs.py

--- hackerrank/divisibleSumPairs.py
+++ hackerrank/divisibleSumPairs.py
@@ -9,8 +9,8 @@ import itertools
 def divisibleSumPairs(n, k, arr):
     count = 0
     for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if((arr[i] + arr[j]) % k == 0):
+        for j in range(i + 1, len(arr)):
+            if (arr[i] + arr[j]) % k == 0:
                 count += 1
 
     return count

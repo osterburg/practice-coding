6233bf9604bdf6cf7246234cc04dbf6497dc5f85
Stephan Osterburg <stephanosterburg@gmail.com>
2019-09-28 11:37:55 -0700

    Update divisibleSumPairs.py
    fixed / simplified

--- hackerrank/divisibleSumPairs.py
+++ hackerrank/divisibleSumPairs.py
@@ -5,21 +5,16 @@ import re
 import sys
 import itertools
 
-# Complete the divisibleSumPairs function below.
+
 def divisibleSumPairs(n, k, arr):
-    m = [0] * len(arr)
-    for e in arr:
-        m[e % k] += 1
-
-    count = int(m[0] * (m[0] - 1)/2)
-    for i in range(1, int(k/2) + 1):
-        if i != k-1:
-            count += m[i] * m[k - i]
-
-    if k % 2 == 0:
-        count += m[int(k/2)] * (m[int(k/2)]-1) / 2
+    count = 0
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if((arr[i] + arr[j]) % k == 0):
+                count += 1
 
     return count
+
 
 if __name__ == '__main__':
     pwd = os.getcwd()

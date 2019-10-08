c2d4a6fcf05db04c061b7f0790f3c891074253cc
Stephan Osterburg <stephanosterburg@gmail.com>
2019-10-08 11:01:42 -0700

    using collections.Counter

--- hackerrank/checkMagazine.py
+++ hackerrank/checkMagazine.py
@@ -2,23 +2,12 @@ import os
 from collections import Counter
 
 def checkMagazine(magazine, note):
-    # print(magazine, '\n', note)
-    # This works but return an error if input is very long:
-    # Terminated due to timeout
-    # for k, v in enumerate(note):
-    #     n = note.count(v)
-    #     m = magazine.count(v)
-    #     if v not in magazine or n > m:
-    #         print('No')
-    #         return
-    #
-    # print('Yes')
-    print(magazine, '\n', note)
     x = Counter(note)
     y = Counter(magazine)
 
-    shared_items = {k: x[k] for k in x if k in y and x[k] != y[k]}
-    if len(shared_items) > 0:
+    overlap = list((x & y).elements())
+
+    if len(note) - len(overlap) > 0:
         print('No')
     else:
         print('Yes')
